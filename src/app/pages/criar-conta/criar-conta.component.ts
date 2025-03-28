bc6d915850808f510e61563f22027c64e04078da
Gustavo <gustavo.ccx@hotmail.com>
2025-03-28 00:00:09 -0300

    SBW-5
    - Ajustado tela de cadastro

--- src/app/pages/criar-conta/criar-conta.component.ts
+++ src/app/pages/criar-conta/criar-conta.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { merge } from 'rxjs';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -8,10 +9,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormBuilder, Validators, FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormBuilder, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
-  standalone: true,
   selector: 'app-criar-conta',
   templateUrl: './criar-conta.component.html',
   styleUrl: './criar-conta.component.scss',
@@ -30,34 +32,51 @@ import { FormBuilder, Validators, FormsModule, ReactiveFormsModule, FormGroup } 
   ],
   providers: [
     provideNgxMask()
-  ]
+  ],
 })
 export class CriarContaComponent {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
 
-    isEditable = true;
-    formGroupDadosPessoais: FormGroup;
-    formGroupLogin: FormGroup;
-
-    constructor(private _formBuilder: FormBuilder) {
-      this.formGroupDadosPessoais = this._formBuilder.group({
-        nome: ['', Validators.required],
-        sobrenome: ['', Validators.required],
-        dataNascimento: ['', Validators.required],
-        genero: ['', Validators.required],
-        estadoCivil: ['', Validators.required],
-        telefone: ['', Validators.required],
-        cpf: ['', Validators.required],
-        email: ['', Validators.required],
-      });
-
-      this.formGroupLogin = this._formBuilder.group({
-        email: ['', Validators.required],
-        senha: ['', Validators.required],
-        confirmarSenha: ['', Validators.required],
-      });
-    };
-
+  emailError = signal('');
   hide = signal(true);
+  isEditable = true;
+
+  formGroupDadosPessoais: FormGroup;
+  formGroupLogin: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.emailErrorMessage());
+
+    this.formGroupDadosPessoais = this._formBuilder.group({
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      dataNascimento: ['', Validators.required],
+      cpf: ['', Validators.required],
+      telefone: ['', Validators.required],
+      profissao: ['', Validators.required],
+      empresa: ['', Validators.required],
+      estadoCivil: ['', Validators.required],
+    });
+
+    this.formGroupLogin = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmarSenha: ['', Validators.required],
+    });
+  }
+
+  emailErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.emailError.set('Por favor, digite um e-mail!');
+    } else if (this.email.hasError('email')) {
+      this.emailError.set('Por favor, digite um e-mail válido!');
+    } else {
+      this.emailError.set('');
+    }
+  }
+
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
