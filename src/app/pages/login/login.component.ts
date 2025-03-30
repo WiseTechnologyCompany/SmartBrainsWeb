0cfcaf3c17a3ffdca7a7b20c22c08a14091d4aca
Gustavo <gustavo.ccx@hotmail.com>
2025-03-30 14:30:59 -0300

    SBW-22
    - Ajustando requisição da tela de Login para o back-end
    - Criando variáveis de ambiente

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,16 @@
 import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { environment } from '../../../environments/environment';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -28,11 +31,11 @@ import { FormControl, FormsModule, ReactiveFormsModule, ValidationErrors, Valida
 export class LoginComponent {
   hide = true;
   formSubmitted = false;
-  private readonly URL = 'http://localhost:8080/SmartBrainsAPI/v1/auth';
-  readonly email = new FormControl('', [Validators.required, Validators.email, this.emailValidador]);
+  private readonly URL = `${environment.API_URL}/auth`;
+  readonly email = new FormControl('', [Validators.required, this.emailValidators]);
   readonly password = new FormControl('', [Validators.required]);
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
   toggleVisibility(event: MouseEvent) {
     this.hide = !this.hide;
@@ -48,26 +51,39 @@ export class LoginComponent {
       return;
     }
 
-    const body = {
+    const body = this.createBody();
+    this.createRequest(body);
+  }
+
+  private createBody() {
+    return {
       email: this.email.value,
       password: this.password.value,
     };
+  }
 
+  private createRequest(body: any) {
     this.httpClient.post(this.URL, body).subscribe(
       (response) => {
         console.log('Login successful', response);
         this.successMessage();
+        this.router.navigate(['/dashboard']);
       },
       (error) => {
         console.error('Login failed', error);
         this.errorMessage();
       }
-    )
+    );
   }
 
-  emailValidador(control: FormControl): ValidationErrors | null {
-   const email = control.value;
-   return email.endsWith('.com') || email.endsWith('.br') ? null : { invalidEmail: true };
+  private emailValidators(control: FormControl): ValidationErrors | null {
+    const email = control.value;
+
+    if (!email.includes('@') || (!email.endsWith('.com') && !email.endsWith('.br'))) {
+      return { invalidEmail: true };
+    }
+
+    return null;
   }
 
   getEmailError(): string {
@@ -75,7 +91,7 @@ export class LoginComponent {
       return 'Por favor, digite um e-mail!';
     }
 
-    if (this.email.hasError('email') || this.email.hasError('invalidEmail')) {
+    if (this.email.hasError('invalidEmail')) {
       return 'Por favor, digite um e-mail válido!';
     }
 
@@ -86,39 +102,43 @@ export class LoginComponent {
     return this.password.hasError('required') ? 'Por favor, digite uma senha!' : '';
   }
 
-  successMessage() {
-    const Toast = Swal.mixin({
+  private successMessage() {
+    Swal.fire({
+      icon: 'success',
+      title: 'Sucesso!',
+      text: 'Login realizado com sucesso!',
       toast: true,
-      position: "top-end",
+      position: 'top-end',
+      background: '#dff0d8', 
+      color: '#000000', 
+      width: '30%',
       showConfirmButton: false,
-      timer: 3000,
+      timer: 4750,
       timerProgressBar: true,
       didOpen: (toast) => {
         toast.onmouseenter = Swal.stopTimer;
         toast.onmouseleave = Swal.resumeTimer;
       }
     });
-    Toast.fire({
-      icon: "success",
-      title: "Login realizado com sucesso!",
+  }
+  
+  private errorMessage() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Credenciais Inválidas!',
+      text: 'Verifique seu e-mail ou senha e tente novamente.',
+      toast: true,
+      position: 'top-end',
+      background: '#f8d7da', 
+      color: '#000000', 
+      width: '30%', 
+      showConfirmButton: false,
+      timer: 4750,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
     });
-  }
-
-  errorMessage() {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.onmouseenter = Swal.stopTimer;
-          toast.onmouseleave = Swal.resumeTimer;
-        }
-      });
-      Toast.fire({
-        icon: "error",
-        title: "Email ou senha incorretos!",
-      });
-  }
+  }  
 }
